5121c8acad5f9cdeb7849b577bfb0dc26d2f93af
MattChris-png <christoffelmatthew@gmail.com>
2024-07-12 03:30:21 -0400

    added services and models for inventory and shoppingcart

---
+++ Everglades.Library/Services/ShoppingCartService.cs
@@ -0,0 +1,43 @@
+﻿using Everglades.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Everglades.Library.Services
+{
+    internal class ShoppingCartService
+    {
+        private static ShoppingCartService? instance;
+        private static object instanceLock  = new object();
+
+        public ReadOnlyCollection<ShoppingCart> carts;
+
+        public ShoppingCart Cart
+        {
+
+            get
+            {
+                if (carts == null)
+                {
+                    return new ShoppingCart();                    
+                }
+                carts.FirstOrDefault();
+            }
+        }
+
+        private ShoppingCartService() { }
+        public static ShoppingCartService Current
+        {
+            get
+            {
+                lock(instanceLock)
+                if(instance == null)
+                    instance = new ShoppingCartService();
+                return instance; 
+            }
+        }
+    }
+}
